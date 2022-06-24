ab8615161fc2ba8be8466a680114c197cf31b026
unknown <muhammadfathullah171217@gmail.com>
2022-06-24 18:43:45 +0700

    crud ruang

--- src/main/java/id/co/mii/ta/clientapp/service/RuangService.java
+++ src/main/java/id/co/mii/ta/clientapp/service/RuangService.java
@@ -5,10 +5,62 @@
  */
 package id.co.mii.ta.clientapp.service;
 
+import id.co.mii.ta.clientapp.model.Ruang;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 /**
  *
  * @author Fathullah
  */
+@Service
 public class RuangService {
+    private RestTemplate restTemplate;
+    
+    @Value("${server.baseUrl}/ruang")
+    private String url;
+
+    @Autowired
+    public RuangService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+    
+     public List<Ruang> getAll(){
+         return restTemplate.exchange(url,
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Ruang>>() {
+        }).getBody();
+    }
+    
+    public Ruang getById(Long id) { // id -> 1 data id
+        return restTemplate.exchange(url.concat("/" + id), // -> pengembalian http kalau getForObject yang disediakan get
+                HttpMethod.GET, null, new ParameterizedTypeReference<Ruang>() {
+        }).getBody();
+    }
+    
+    public Ruang createRuang(Ruang ruang) { // create -> data
+        return restTemplate.exchange(url,
+                HttpMethod.POST,
+                new HttpEntity(ruang), // -> requestbody dri method sama seperti postman 
+                new ParameterizedTypeReference<Ruang>() { // -> tipe pengembalian dri backend karna dri back end region maka typenya region
+        }).getBody(); // -> ngambil isi respon
+    }
+
+    public Ruang updateRuang(Long id, Ruang ruang) { // update ->, id -> mengambil data, Region -> data
+        return restTemplate.exchange(url.concat("/" + id),
+                HttpMethod.PUT, new HttpEntity(ruang), new ParameterizedTypeReference<Ruang>() {
+        }).getBody();
+    }
+
+    public Ruang deleteRuang(Long id) {// delete -> yang dibutuhkan id
+        return restTemplate.exchange(url.concat("/" + id),
+                HttpMethod.DELETE, null, new ParameterizedTypeReference<Ruang>() {
+        }).getBody();
+    }
     
 }
---
+++ src/main/resources/static/js/ruang.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+    $("#tbEMP").DataTable({
+        "ajax": {
+            "url": "/ruang/get-all",
+            "dataSrc": ""
+        },
+        "columns": [{
+                "data": "id"
+            },
+            {
+                "data": "name"
+            },
+            {
+                "data": "lantai"
+            },
+            {
+                "data": null,
+                render: function (data, type, row, meta) {
+                    return `<button class="btn btn-rounded btn-primary" onclick = "modalRuang(${data.id})" data-bs-toggle="modal" data-bs-target="#detailRuang"><span class="btn-icon-start text-primary"><i class="fa fa-info"></i>
+                        </span>Detail</button>
+                        <button class="btn btn-rounded btn-danger" onclick="deleteRuang(${data.id})"><span class="btn-icon-start text-danger"><i class="fa fa-trash"></i></span>Delete</button>`
+                }
+            }
+        ],
+        language: {
+            paginate: {
+                next: '<i class="fa fa-angle-double-right" aria-hidden="true"></i>',
+                previous: '<i class="fa fa-angle-double-left" aria-hidden="true"></i>'
+            }
+        }
+        // console.log()
+    });
+});
+
+function modalRuang(id) {
+    $.ajax({
+        type: 'GET',
+        url: "/ruang/getById/" + id,
+        dataType: 'json',
+        contentType: ''
+    }).done((result) => {
+        $('#id').text(result.id);
+        $('#name').text(result.name);
+        $('#lantai').text(result.lantai);
+    }).fail((error) => {
+        console.log(error);
+    });
+}
+
+$('#createRuang').click(function (e) { //modal btn save
+    let name = $('#nameInp').val()
+    let lantai = $('#lantaiInp').val()
+    $.ajax({
+        method: "POST",
+        url: "ruang/createRuang",
+        dataType: "json",
+        beforeSend: addCsrfToken(),
+        data: JSON.stringify({
+            name: name,
+            lantai: lantai
+        }),
+        contentType: "application/json",
+        success: function (result) {
+            $('#tbEMP').DataTable().ajax.reload()
+            $("#addRuang").modal('hide')
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Ruang has been created',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+    })
+})
+
+function deleteRuang(id) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't delete this Ruang!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                method: "DELETE",
+                url: "ruang/deleteRuang/" + id,
+                dataType: "json",
+                beforeSend: addCsrfToken(),
+                contentType: "application/json",
+                success: (result) => {
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                    $('#tbEMP').DataTable().ajax.reload()
+                }
+            })
+        }
+    })
+}
