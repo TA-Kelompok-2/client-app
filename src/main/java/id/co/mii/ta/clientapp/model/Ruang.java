4e93183f68f55759afcc2bec4bcc55b5d43ee8f4
unknown <muhammadfathullah171217@gmail.com>
2022-06-24 18:21:35 +0700

    .

---
+++ src/main/java/id/co/mii/ta/clientapp/model/Ruang.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.co.mii.ta.clientapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Fathullah
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ruang {
+
+    private Long id;
+    private String name;
+    private String lantai;
+}
