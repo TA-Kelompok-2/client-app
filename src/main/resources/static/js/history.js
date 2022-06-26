42dd3cd2ac1666074581cd4a806bb2d3167398b2
unknown <muhammadfathullah171217@gmail.com>
2022-06-27 01:36:59 +0700

    fix bug

--- src/main/resources/static/js/history.js
+++ src/main/resources/static/js/history.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
                 }
             },
             {
-                "data": "request.employee.phoneNumber"
+                "data": "request.id"
             },
             {
                 "data": "request.date"
@@ -54,7 +54,7 @@ function modalHistory(id) {
         contentType: ''
     }).done((result) => {
         $('#name').text(result.request.employee.name);
-        $('#phoneNumber').text(result.request.employee.phoneNumber);
+        $('#request_id').text(result.request.id);
         $('#date').text(result.request.date);
         $('#fasilitas').text(result.request.fasilitasRuang.fasilitas.name);
         $('#ruang').text(result.request.fasilitasRuang.ruang.name);
@@ -62,69 +62,6 @@ function modalHistory(id) {
     }).fail((error) => {
         console.log(error);
     });
-}
-
-$('#createEmployee').click(function (e) { //modal btn save
-    let name = $('#nameInp').val()
-    let email = $('#emailInp').val()
-    let phoneNumber = $('#phoneNumberInp').val()
-    let username = $('#usernameInp').val()
-    let password = $('#passwordInp').val()
-    $.ajax({
-        method: "POST",
-        url: "employee/createEmployee",
-        dataType: "json",
-        beforeSend: addCsrfToken(),
-        data: JSON.stringify({
-            name: name,
-            email: email,
-            phoneNumber: phoneNumber,
-            username: username,
-            password: password,
-        }),
-        contentType: "application/json",
-        success: function (result) {
-            $('#tbEMP').DataTable().ajax.reload()
-            $("#addEmployee").modal('hide')
-            Swal.fire({
-                position: 'center',
-                icon: 'success',
-                title: 'Employee has been created',
-                showConfirmButton: false,
-                timer: 1500
-            })
-        }
-    })
-})
-
-function deleteEmployee(id) {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't delete this employee!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                method: "DELETE",
-                url: "employee/deleteEmployee/" + id,
-                dataType: "json",
-                beforeSend: addCsrfToken(),
-                contentType: "application/json",
-                success: (result) => {
-                    Swal.fire(
-                        'Deleted!',
-                        'Your file has been deleted.',
-                        'success'
-                    )
-                    $('#tbEMP').DataTable().ajax.reload()
-                }
-            })
-        }
-    })
 }
 
 function approve(id) {
