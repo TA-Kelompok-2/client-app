bf0d32b123a705139e3426a6cff8c7ea3ba5c63f
unknown <muhammadfathullah171217@gmail.com>
2022-06-28 16:32:10 +0700

    filtering dropdown selected

---
+++ src/main/resources/static/js/request.js
@@ -0,0 +1,50 @@
+// Map your choices to your option value
+var lookup = {
+    'Option 1': ['Option 1 - Choice 1', 'Option 1 - Choice 2', 'Option 1 - Choice 3'],
+    'Option 2': ['Option 2 - Choice 1', 'Option 2 - Choice 2'],
+    'Option 3': ['Option 3 - Choice 1'],
+};
+
+// When an option is changed, search the above for matching choices
+$('#options').on('change', function () {
+    // Set selected option as variable
+    var selectValue = $(this).val();
+    console.log(selectValue)
+
+    // Empty the target field
+    $('#choices').empty();
+
+    // For each chocie in the selected option
+    for (i = 0; i < lookup[selectValue].length; i++) {
+        // Output choice in the target field
+        $('#choices').append("<option value='" + lookup[selectValue][i] + "'>" + lookup[selectValue][i] + "</option>");
+    }
+});
+
+// Map your choices to your option value
+var lookup = {
+    'Option 1': ['Option 1 - Choice 1', 'Option 1 - Choice 2', 'Option 1 - Choice 3'],
+    'Option 2': ['Option 2 - Choice 1', 'Option 2 - Choice 2'],
+    'Option 3': ['Option 3 - Choice 1'],
+    // idruang : [idfasilitasruang]
+};
+
+// var tes = $('#ruang').on('change', function () {
+//     var selectValue = $(this).val();
+// )}
+
+// When an option is changed, search the above for matching choices
+$('#ruang').on('change', function () {
+    // Set selected option as variable
+    var selectValuem = $(this).val();
+    console.log(selectValuem)
+
+    // Empty the target field
+    $('#fasilitasruang').empty();
+
+    // For each chocie in the selected option
+    for (i = 0; i < lookup[selectValue].length; i++) {
+        // Output choice in the target field
+        $('#fasilitasruang').append("<option value='" + lookup[selectValue][i] + "'>" + lookup[selectValue][i] + "</option>");
+    }
+});
